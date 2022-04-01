ab43d596f9f0ccfba2fdaa3a18246b0130b53d0f
Ali Gamal Ali <hictors@gmail.com>
2022-04-01 11:23:38 +0200

    Finishing the design

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,23 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    authModalShow: false,
+  },
+
+  getters: {
+    // we did comment this coz we can return mapState instead of mapGetters function coz we dont't perform any operation
+    // on the getter it's over kill in this situation
+    // authModalShow: (state) => state.authModalShow,
+  },
+
+  mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+
   actions: {},
+
   modules: {},
 });
