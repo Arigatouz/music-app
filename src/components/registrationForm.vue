2518f03ff28d22e332572d54e7ca4d251ca5ade5
Ali Gamal Ali <hictors@gmail.com>
2022-05-02 22:51:06 +0200

    finishing the registration form

--- src/components/registrationForm.vue
+++ src/components/registrationForm.vue
@@ -9,19 +9,26 @@ export default {
         name: "required|min:4|max:25|alpha_spaces",
         email: "required|email",
         age: "required|min_value:15|max_value:100",
-        password: "required|min:3|max:25",
+        password: "required|min:3|max:25|alpha_spaces",
         confirm_password: "required|password_mismatch:@password",
         country: "required|excluded_country:Israel",
         tos: "tos",
       },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_message: "Please wait! your account is being created!",
+      reg_alert_variant: "bg-blue-600",
     };
   },
   methods: {
     // register
     register(values) {
-      console.log(values);
-    },
-    logErrorsArray(values) {
+      this.reg_in_submission = true;
+      this.reg_alert_variant = "bg-blue-600";
+      this.reg_show_alert = true;
+      this.reg_alert_message = "Please wait! your account is being created!";
+      this.reg_alert_variant = "bg-blue-600";
+      this.reg_alert_message = "Success. you account has been created!";
       console.log(values);
     },
   },
@@ -29,103 +36,113 @@ export default {
 </script>
 
 <template>
-  <vee-form
-    :validation-schema="schema"
-    @submit="register"
-    :initial-values="{ userData }"
-  >
-    <!-- Name -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Name</label>
-      <vee-field
-        type="text"
-        name="name"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        placeholder="Enter Name"
-      />
-      <error-message name="name" class="text-red-600" />
-    </div>
-    <!-- Email -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Email</label>
-      <vee-field
-        name="email"
-        type="email"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        placeholder="Enter Email"
-      />
-      <error-message name="email" class="text-red-600" />
-    </div>
-    <!-- Age -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Age</label>
-      <vee-field
-        name="age"
-        type="number"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-      />
-      <error-message name="age" class="text-red-600" />
-    </div>
-    <!-- Password -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Password</label>
-      <vee-field name="password" :bails="false" v-slot="{ field, errors }">
-        <input
-          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-          type="password"
-          placeholder="Password"
-          v-bind="field"
-          @click.prevent="logErrorsArray(errors)"
-        />
-        <div class="text-red-600" v-for="error in errors" :key="error">
-          <p>{{ error }}</p>
-        </div>
-      </vee-field>
-    </div>
-    <!-- Confirm Password -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Confirm Password</label>
-      <vee-field
-        name="confirm_password"
-        type="password"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        placeholder="Confirm Password"
-      />
-      <error-message name="confirm_password" class="text-red-600" />
-    </div>
-    <!-- Country -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Country</label>
-      <vee-field
-        as="select"
-        name="country"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-      >
-        <option value="USA">USA</option>
-        <option value="Mexico">Mexico</option>
-        <option value="Germany">Germany</option>
-        <option value="Israel">Israel</option>
-      </vee-field>
-      <error-message name="country" class="text-red-600" />
-    </div>
-    <!-- TOS -->
-    <div class="mb-3 pl-6">
-      <vee-field
-        name="tos"
-        value="1"
-        type="checkbox"
-        class="w-4 h-4 float-left -ml-6 mt-1 rounded"
-      />
-      <label class="inline-block">Accept terms of service</label>
-      <error-message name="tos" class="text-red-600 block" />
-    </div>
-    <button
-      type="submit"
-      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+  <div>
+    <div
+      class="text-white text-center font-bold p-4 mb-4"
+      v-if="reg_show_alert"
+      :class="reg_alert_variant"
     >
-      Submit
-    </button>
-  </vee-form>
+      <p>{{ reg_alert_message }}</p>
+    </div>
+
+    <vee-form
+      :validation-schema="schema"
+      @submit="register"
+      :initial-values="userData"
+    >
+      <!-- Name -->
+      <div class="mb-3">
+        <label class="inline-block mb-2">Name</label>
+        <vee-field
+          type="text"
+          name="name"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          placeholder="Enter Name"
+        />
+        <error-message name="name" class="text-red-600" />
+      </div>
+      <!-- Email -->
+      <div class="mb-3">
+        <label class="inline-block mb-2">Email</label>
+        <vee-field
+          name="email"
+          type="email"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          placeholder="Enter Email"
+        />
+        <error-message name="email" class="text-red-600" />
+      </div>
+      <!-- Age -->
+      <div class="mb-3">
+        <label class="inline-block mb-2">Age</label>
+        <vee-field
+          name="age"
+          type="number"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        />
+        <error-message name="age" class="text-red-600" />
+      </div>
+      <!-- Password -->
+      <div class="mb-3">
+        <label class="inline-block mb-2">Password</label>
+        <vee-field name="password" v-slot="{ field, errors }">
+          <input
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            type="password"
+            placeholder="Password"
+            v-bind="field"
+          />
+          <div class="text-red-600" v-for="error in errors" :key="error">
+            <p>{{ error }}</p>
+          </div>
+        </vee-field>
+      </div>
+      <!-- Confirm Password -->
+      <div class="mb-3">
+        <label class="inline-block mb-2">Confirm Password</label>
+        <vee-field
+          name="confirm_password"
+          type="password"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          placeholder="Confirm Password"
+        />
+        <error-message name="confirm_password" class="text-red-600" />
+      </div>
+      <!-- Country -->
+      <div class="mb-3">
+        <label class="inline-block mb-2">Country</label>
+        <vee-field
+          as="select"
+          name="country"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        >
+          <option value="USA">USA</option>
+          <option value="Mexico">Mexico</option>
+          <option value="Germany">Germany</option>
+          <option value="Israel">Israel</option>
+        </vee-field>
+        <error-message name="country" class="text-red-600" />
+      </div>
+      <!-- TOS -->
+      <div class="mb-3 pl-6">
+        <vee-field
+          name="tos"
+          value="1"
+          type="checkbox"
+          class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+        />
+        <label class="inline-block">Accept terms of service</label>
+        <error-message name="tos" class="text-red-600 block" />
+      </div>
+      <button
+        :disabled="reg_in_submission"
+        type="submit"
+        class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+      >
+        Submit
+      </button>
+    </vee-form>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/plugins/formValidation.js
+++ src/plugins/formValidation.js
@@ -16,6 +16,7 @@ import {
   min_value,
   confirmed,
   is_not,
+  one_of,
 } from "@vee-validate/rules";
 
 export default {
@@ -35,29 +36,36 @@ export default {
     defineRule("password_mismatch", confirmed);
     defineRule("excluded", is_not);
     defineRule("excluded_country", is_not);
+    defineRule("special_character", one_of);
 
     configure({
-      // bails: false,
+      bails: false,
       generateMessage: (ctx) => {
         const messages = {
           required: `the field ${ctx.field} is required`,
-          min: `the field ${ctx.field} is too short`,
-          max: `the field ${ctx.field} is too long `,
-          email: `the field ${ctx.field} need to have valid email`,
-          alpha_spaces: `the field ${ctx.field} must contain letters and spaces only!`,
-          max_value: `the field ${ctx.field} is too heigh`,
-          min_value: `the field ${ctx.field} is too low`,
+          min: `The field ${ctx.field} is too short`,
+          max: `The field ${ctx.field} is too long `,
+          email: `The field ${ctx.field} need to have valid email`,
+          alpha_spaces: `The field ${ctx.field} must contain letters and spaces only!`,
+          max_value: `The field ${ctx.field} is too heigh`,
+          min_value: `The field ${ctx.field} is too low`,
           password_mismatch: `The password you entered don't match`,
           excluded: `You are not allowed to use this value for the field ${ctx.field}.`,
           excluded_country:
             "Due to restrictions, we do not accept users from this location.",
-          tos: `you must select the terms of service before continue on registration`,
+          tos: `you must select the terms of service `,
+          special_character:
+            "Password needs to have at least one special Character",
         };
         const message = messages[ctx.rule.name]
           ? messages[ctx.rule.name]
           : `The field ${ctx.field} is invalid.`;
         return message;
       },
+      validateOnBlur: true, //validation on clicking on the input and then click away
+      validateOnModelUpdate: true, // validation if the value changes internally in the v-model directive
+      validateOnChange: true, // validate when you change happens in an input
+      validateOnInput: false, // validate on every key stroke this is by far the most aggressive in validation
     });
   },
 };
